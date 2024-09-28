3ebd2af5de4744445c33e4775ad8d48f6baec9ab
Adriele <adriele.dejprimo@gmail.com>
2024-09-28 09:33:11 -0300

    Function Saving pdf to Excel

--- converter.py
+++ converter.py
@@ -2,7 +2,7 @@ import camelot
 import pandas as pd
 import tabula   
 
-def extractTabularTables(pdfPath):
+def extractTabulaTables(pdfPath):
     try:
         tables = tabula.read_pdf(pdfPath, pages='all', multiple_tables=True)
         return tables
@@ -18,3 +18,11 @@ def extractCamelotTables(pdfPath):
         print(f'Error when extracting with Camelot: {e}')
         return None
     
+def savePdfToExcel(tables, fileNameExcel):
+    with pd.ExcelWriter(fileNameExcel, engine='openpyxl') as writer:
+        for idx, table in enumerate(tables):
+            table.to_excel(writer, sheet_name=f'Table_{idx+1}', index=False)
+        print(f'Conversion complete! File saved as {fileNameExcel}')
+
+
+
