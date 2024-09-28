6a86763ba3c8a206c3e59a38c0af93b0402514be
Adriele <adriele.dejprimo@gmail.com>
2024-09-28 10:36:30 -0300

    Fixing problems

--- converter.py
+++ converter.py
@@ -13,7 +13,7 @@ def extractTabulaTables(pdfPath):
 def extractCamelotTables(pdfPath):
     try:
         tables = camelot.read_pdf(pdfPath, pages='all')
-        return [tables.df for table in tables]
+        return [table.df for table in tables]
     except Exception as e:
         print(f'Error when extracting with Camelot: {e}')
         return None
