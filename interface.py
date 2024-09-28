69c64c40b9c722be67fc350f7c14c3bba99bcc0f
Adriele <adriele.dejprimo@gmail.com>
2024-09-28 12:05:09 -0300

    A real interface

--- interface.py
+++ interface.py
@@ -28,3 +28,19 @@ def converter():
         convertPdfToExcel(pdfPath, excelFile)
         showinfo('Success', f'Conversion Complete! File saved as {excelFile}')
     
+app = Tk()
+app.title('PDF to Excel converter')
+app.geometry('400x200')
+
+selectedPdfPath = StringVar()
+
+btnChooseFile = Button(app, text='Choose PDF file', command=chooseFile)
+btnChooseFile.pack(pady=20)
+
+labelFile = Label(app, text='No files selected')
+labelFile.pack(pady=10)
+
+btnConverter = Button(app, text='Convert to Excel', command=converter)
+btnConverter.pack(pady=20)
+
+app.mainloop()
